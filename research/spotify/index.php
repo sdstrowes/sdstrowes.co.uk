5f82d35dcf1047e755f23b863987a53478356bd3
Stephen D. Strowes <sdstrowes@gmail.com>
2014-10-26 03:26:37 +0000

    add the spotify page

---
+++ research/spotify/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title>Stephen D. Strowes</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<link rel="stylesheet" href="http://www.sdstrowes.co.uk/style.css" type="text/css" media="all" />
+</head>
+
+<body>
+
+<?php include( "../../header.php" ) ?>
+
+<div id="content">
+
+<h3>An Experimental Study of Client-Side Spotify Peering Behaviour</h3>
+
+<p>The following are links to the Spotify playlists we used in this study:</p>
+
+<ul>
+
+<li>100-random: <a
+href="http://open.spotify.com/user/sdstrowes/playlist/65Mn8Lod4Usu1BNQv7iGih">http://open.spotify.com/user/sdstrowes/playlist/65Mn8Lod4Usu1BNQv7iGih</a></li>
+
+<li>100-rollingstone
+<a href="http://open.spotify.com/user/sdstrowes/playlist/2tp4suurKex6PV31JsoIQk">http://open.spotify.com/user/sdstrowes/playlist/2tp4suurKex6PV31JsoIQk</a></li>
+
+<li>500-rollingstone
+<a href="http://open.spotify.com/user/sdstrowes/playlist/2OV5pMk3FP4Gp83rGwDRCU">http://open.spotify.com/user/sdstrowes/playlist/2OV5pMk3FP4Gp83rGwDRCU</a></li>
+
+</ul>
+
+</div>
+
+<?php include( "../../copyright.php" ) ?>
+
+</body>
+
+</html>
